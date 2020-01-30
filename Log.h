cc9507a3d02633235981fe19dc2358053fa2b160
ardazishvili <ardazishvili.roman@yandex.ru>
2020-01-30 18:48:21 +0400

    add macros for simplified logging

--- Log.h
+++ Log.h
@@ -25,4 +25,18 @@ private:
 
 std::unique_ptr<Log> createSpdlog();
 
+#define FG_CORE_TRACE(...) ::Log::logCore(Level::Trace, __VA_ARGS__);
+#define FG_CORE_DEBUG(...) ::Log::logCore(Level::Debug, __VA_ARGS__);
+#define FG_CORE_INFO(...) ::Log::logCore(Level::Info, __VA_ARGS__);
+#define FG_CORE_WARN(...) ::Log::logCore(Level::Warn, __VA_ARGS__);
+#define FG_CORE_ERROR(...) ::Log::logCore(Level::Error, __VA_ARGS__);
+#define FG_CORE_CRITICAL(...) ::Log::logCore(Level::Critical, __VA_ARGS__);
+
+#define FG_APP_TRACE(...) ::Log::logApp(Level::Trace, __VA_ARGS__);
+#define FG_APP_DEBUG(...) ::Log::logApp(Level::Debug, __VA_ARGS__);
+#define FG_APP_INFO(...) ::Log::logApp(Level::Info, __VA_ARGS__);
+#define FG_APP_WARN(...) ::Log::logApp(Level::Warn, __VA_ARGS__);
+#define FG_APP_ERROR(...) ::Log::logApp(Level::Error, __VA_ARGS__);
+#define FG_APP_CRITICAL(...) ::Log::logApp(Level::Critical, __VA_ARGS__);
+
 #endif
