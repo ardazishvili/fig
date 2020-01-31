48fb56c6285fd9d9648b0529a3a86dc192de1378
ardazishvili <ardazishvili.roman@yandex.ru>
2020-01-31 20:14:04 +0400

    add Core class with logger

--- Application.h
+++ Application.h
@@ -32,14 +32,6 @@ protected:
   Log<T> _log;
 };
 
-/* #define FG_CORE_TRACE(...) gApp()->logCore(Level::Trace, __VA_ARGS__); */
-/* #define FG_CORE_DEBUG(...) gApp()->logCore(Level::Debug, __VA_ARGS__); */
-/* #define FG_CORE_INFO(...) gApp()->logCore(Level::Info, __VA_ARGS__); */
-/* #define FG_CORE_WARN(...) gApp()->logCore(Level::Warn, __VA_ARGS__); */
-/* #define FG_CORE_ERROR(...) gApp()->logCore(Level::Error, __VA_ARGS__); */
-/* #define FG_CORE_CRITICAL(...) gApp()->logCore(Level::Critical, __VA_ARGS__);
- */
-
 #define FG_APP_TRACE(...) gApp()->log(Level::Trace, __VA_ARGS__);
 #define FG_APP_DEBUG(...) gApp()->log(Level::Debug, __VA_ARGS__);
 #define FG_APP_INFO(...) gApp()->log(Level::Info, __VA_ARGS__);
