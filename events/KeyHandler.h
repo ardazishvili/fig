3f9d25655daac32befb63d6f8c398b2d63b31ef3
ardazishvili <ardazishvili.roman@yandex.ru>
2022-03-07 19:32:14 +0300

    Allow to get the list of modified key's handlers

--- events/KeyHandler.h
+++ events/KeyHandler.h
@@ -9,14 +9,23 @@ namespace fig {
 using KeyboardKey = int;
 using KeyHandlerFunction = std::function<void(EventManager*)>;
 
-class KeyHander {
+struct Action {
+  KeyHandlerFunction action;
+  std::string description;
+  bool isCustom{false};
+};
+
+using ActionMap = std::map<KeyboardKey, Action>;
+
+class KeyHandler {
  public:
-  KeyHander();
+  KeyHandler();
   void process(KeyboardKey key, EventManager* m);
   void setHandler(KeyboardKey, KeyHandlerFunction f, std::string description);
+  ActionMap getCustomHandlers() const;
 
  private:
-  std::map<KeyboardKey, std::pair<KeyHandlerFunction, std::string> > _handlers;
+  ActionMap _handlers;
 };
 
 }  // namespace fig
